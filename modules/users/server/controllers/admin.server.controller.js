5ab80fa39f4612ddc92d4a00e388b7d44cb52c7c
Matthew D Webb <matt.d.webb@icloud.com>
2016-03-18 19:15:19 +0000

    user object updates

--- modules/users/server/controllers/admin.server.controller.js
+++ modules/users/server/controllers/admin.server.controller.js
@@ -25,6 +25,11 @@ exports.update = function (req, res) {
   user.firstName = req.body.firstName;
   user.lastName = req.body.lastName;
   user.displayName = user.firstName + ' ' + user.lastName;
+  user.age = req.body.age;
+  user.dob = req.body.dob;
+  user.medical = req.body.medical;
+  user.bibColor = req.body.bibColor;
+  user.location = req.body.location;
   user.roles = req.body.roles;
 
   user.save(function (err) {
